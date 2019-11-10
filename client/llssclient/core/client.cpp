ae4d48c497045e9999eee0b999f663a7b447b7dc
Shampooh <agent.serega@yandex.ru>
2019-11-10 13:20:25 +0300

    Add dummy source files

--- client/llssclient/core/client.cpp
+++ client/llssclient/core/client.cpp
@@ -1,1 +1,24 @@
 #include "llssclient/core/client.h"
+
+failless::client::client::Client::Client(failless::client::mp_client::MpClient *mp_client,
+                                         failless::client::filesystem::FileSystem *filesystem, string config) {
+
+}
+
+size_t failless::client::client::Client::ReadQuery(string query) {
+    return 0;
+}
+
+size_t failless::client::client::Client::Send() {
+    return 0;
+}
+
+size_t failless::client::client::Client::ParseQuery_(std::string query) {
+    return 0;
+}
+
+size_t failless::client::client::Client::Params(int argc, char **argv) {
+    return 0;
+}
+
+
--- client/llssclient/network/mp_client.cpp
+++ client/llssclient/network/mp_client.cpp
@@ -1,1 +1,30 @@
 #include "llssclient/network/mp_client.h"
+
+#include <utility>
+
+failless::client::mp_client::MpClient::MpClient() {
+
+}
+
+failless::client::mp_client::MpClient::~MpClient() = default;
+
+void failless::client::mp_client::MpClient::Stop() {
+
+}
+
+size_t failless::client::mp_client::MpClient::ReadComplete_(const boost::system::error_code &err, size_t bytes) {
+    return 0;
+}
+
+bool failless::client::mp_client::MpClient::Started() {
+    return false;
+}
+
+void failless::client::mp_client::MpClient::Start(const boost::asio::ip::tcp::endpoint& ep) {
+
+}
+
+failless::client::mp_client::MpClient::ptr
+failless::client::mp_client::MpClient::Start(const boost::asio::ip::tcp::endpoint& ep, const std::string &username) {
+    return failless::client::mp_client::MpClient::ptr();
+}
