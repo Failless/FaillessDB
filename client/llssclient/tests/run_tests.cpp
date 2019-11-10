4e725830d9ff2bdf07c8e8ef45c1fcb45deeab81
Shampooh <agent.serega@yandex.ru>
2019-11-10 13:21:24 +0300

    Rewrite mock testing for Test function in Client interface

--- client/llssclient/tests/run_tests.cpp
+++ client/llssclient/tests/run_tests.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include "llssclient/core/client.h"
-#include "llssclient/network/mp_client.h"
 #include "llssclient/filesystem/filesystem.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -14,42 +13,23 @@ namespace failless::client::tests {
 
     using std::string;
 
-    using namespace failless::client::client_core;
+    using namespace failless::client::client;
 
-    class mock_client : public client_interface {
+    class MockClient : public ClientInterface {
     public:
-
-        MOCK_METHOD1(read_query, size_t(string
-                query ));
-
-        MOCK_METHOD0(send, size_t());
-
-        MOCK_METHOD2(params, size_t(int
-                argc, char * *arg ));
+        MockClient() : ClientInterface() {};
+        MOCK_METHOD1(ReadQuery, size_t(string  query));
+        MOCK_METHOD0(Send, size_t());
+        MOCK_METHOD2(Params, size_t(int argc, char **arg));
     private:
-        MOCK_METHOD1(parse_query, size_t(string
-                query ));
+        MOCK_METHOD1(ParseQuery_, size_t(string query));
     };
 
-    class best_client {
-        mock_client *client;
-    public:
-        best_client(mock_client *client) : client(client) {}
-
-        bool get_data(string) {
-            string test_query = "test";
-            client->read_query(test_query);
-            return true;
-        }
-    };
-
-    TEST(best_client_test, can_read_query) {
-        mock_client mock_client;
+    TEST(client_test, can_read_query) {
+        MockClient mock_client;
         string test_query = "test";
-        EXPECT_CALL(mock_client, read_query(test_query)).Times(AtLeast(1));
-
-        best_client best_client(&mock_client);
-
+        EXPECT_CALL(mock_client, ReadQuery(test_query)).Times(AtLeast(1));
+        EXPECT_EQ(mock_client.Test("test"), EXIT_SUCCESS);
     }
 
     int main(int argc, char **argv) {
