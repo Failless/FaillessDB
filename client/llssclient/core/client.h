3e1136cbe6ffbb9d2a1c77e6129858b3d729c8c1
Shampooh <agent.serega@yandex.ru>
2019-11-10 13:38:08 +0300

    Fix indents in header files

--- client/llssclient/core/client.h
+++ client/llssclient/core/client.h
@@ -10,41 +10,41 @@
 
 namespace failless::client::client {
 
-    using std::string;
+using std::string;
 
-    using namespace failless::client::mp_client;
-    using namespace failless::client::filesystem;
+using namespace failless::client::mp_client;
+using namespace failless::client::filesystem;
 
-    class ClientInterface {
-    public:
-        ClientInterface() = default;
-        ~ClientInterface() = default;
-        virtual size_t ReadQuery( string query ) = 0;
-        virtual size_t Send() = 0;
-        virtual size_t Params( int argc, char **argv ) = 0;
-        int Test(string query) {
-            ReadQuery(query);
-            return EXIT_SUCCESS;
-        }
-    private:
-        virtual size_t ParseQuery_( string query ) = 0;
-    };
+class ClientInterface {
+public:
+    ClientInterface() = default;
+    ~ClientInterface() = default;
+    virtual size_t ReadQuery( string query ) = 0;
+    virtual size_t Send() = 0;
+    virtual size_t Params( int argc, char **argv ) = 0;
+    int Test(string query) {
+        ReadQuery(query);
+        return EXIT_SUCCESS;
+    }
+private:
+    virtual size_t ParseQuery_( string query ) = 0;
+};
 
-    class Client : public ClientInterface {
-    public:
-        Client() = default;
-        Client(MpClient* mp_client, FileSystem* filesystem, string config);
-        ~Client() = default;
-        size_t ReadQuery( string query ) override;
-        size_t Send() override;
-        size_t Params( int argc, char **argv ) override;
-    private:
-        size_t ParseQuery_( string query ) override;
-    private:
-        MpClient* mp_client_;
-        FileSystem* filesystem_;
-        string config_;
-    };
+class Client : public ClientInterface {
+public:
+    Client() = default;
+    Client(MpClient* mp_client, FileSystem* filesystem, string config);
+    ~Client() = default;
+    size_t ReadQuery( string query ) override;
+    size_t Send() override;
+    size_t Params( int argc, char **argv ) override;
+private:
+    size_t ParseQuery_( string query ) override;
+private:
+    MpClient* mp_client_;
+    FileSystem* filesystem_;
+    string config_;
+};
 
 }
 #endif // LLSSCLIENT_CORE_CLIENT_H_
--- client/llssclient/network/mp_client.h
+++ client/llssclient/network/mp_client.h
@@ -11,33 +11,33 @@
 
 namespace failless::client::mp_client {
 
-    using namespace boost::asio;
+using namespace boost::asio;
 
-    const int MAX_MSG = 1024;
+const int MAX_MSG = 1024;
 
-    class MpClientInterface {};
+class MpClientInterface {};
 
-    class MpClient : public MpClientInterface {
-    public:
-        explicit MpClient();
+class MpClient : public MpClientInterface {
+public:
+    explicit MpClient();
 
-        ~MpClient();
-        static void Start(const ip::tcp::endpoint& ep);
+    ~MpClient();
+    static void Start(const ip::tcp::endpoint& ep);
 
-        typedef boost::system::error_code error_code;
-        typedef boost::shared_ptr<MpClient> ptr;
+    typedef boost::system::error_code error_code;
+    typedef boost::shared_ptr<MpClient> ptr;
 
-        static ptr Start(const ip::tcp::endpoint& ep, const std::string &username);
-        static void Stop();
-        static bool Started();
+    static ptr Start(const ip::tcp::endpoint& ep, const std::string &username);
+    static void Stop();
+    static bool Started();
 
-    private:
-        static size_t ReadComplete_(const boost::system::error_code &err, size_t bytes);
+private:
+    static size_t ReadComplete_(const boost::system::error_code &err, size_t bytes);
 
-    private:
-        char read_buffer_[MAX_MSG]{};
-        char write_buffer_[MAX_MSG]{};
-    };
+private:
+    char read_buffer_[MAX_MSG]{};
+    char write_buffer_[MAX_MSG]{};
+};
 }
 
 #endif // LLSSCLIENT_NETWORK_MP_CLIENT_H_
