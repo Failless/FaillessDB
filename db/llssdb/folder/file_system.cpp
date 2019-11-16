820185651d22f92ce520cedc9cd4c6624fbe85cd
Egor Bedov <bedovegor2203@gmail.com>
2019-11-16 13:15:42 +0300

    ITaskManager tests

---
+++ db/llssdb/folder/file_system.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Егор Бедов on 16/11/2019.
+//
+
+#include "file_system.h"
---
+++ db/llssdb/folder/file_system.h
@@ -0,0 +1,14 @@
+//
+// Created by Егор Бедов on 16/11/2019.
+//
+
+#ifndef FAILLESS_FILE_SYSTEM_H
+#define FAILLESS_FILE_SYSTEM_H
+
+
+class file_system {
+
+};
+
+
+#endif //FAILLESS_FILE_SYSTEM_H
