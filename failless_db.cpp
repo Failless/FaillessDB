eb30ab14ea1fad84858b7b390afda13a1bd97386
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-11-20 12:11:14 +0300

    fixed linking error with rocksdb

--- failless_db.cpp
+++ failless_db.cpp
@@ -1,5 +1,9 @@
+#include "llssdb/folder/data_worker.h"
 #include <iostream>
+#include <memory>
 
 int main(int argc, char **argv) {
+  std::shared_ptr<IDataWorker> data_worker(new DataWorker());
+  data_worker->Create();
   return 0;
 }
