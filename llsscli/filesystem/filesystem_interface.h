c2a4ba1df3924c4f8abd3ba89d543adfcdb6549a
Shampooh <agent.serega@yandex.ru>
2019-11-25 05:53:05 +0300

    Fix small namespace issues in filesystem interface

--- llsscli/filesystem/filesystem_interface.h
+++ llsscli/filesystem/filesystem_interface.h
@@ -1,18 +1,29 @@
 #ifndef FAILLESS_FILESYSTEM_INTERFACE_H
 #define FAILLESS_FILESYSTEM_INTERFACE_H
 
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "llsscli/config/config.h"
+
 namespace failless {
 namespace client {
 namespace filesystem {
+
+using std::string;
+using std::vector;
+
+using namespace failless::client::config;
 
 class FileSystemInterface {
 public:
     FileSystemInterface() = default;
     ~FileSystemInterface() = default;
 
-    virtual size_t ReadFile(uintptr_t data) = 0;
-
+    virtual size_t ReadFile(string file_path) = 0;
     virtual size_t WriteFile(uintptr_t data) = 0;
+    virtual size_t CleanPayload() = 0;
 };
 
 } // namespace filesystem
