dbeff0cc82664ce9f1977c9cc9d3630280a00485
Shampooh <agent.serega@yandex.ru>
2019-11-28 20:26:57 +0300

    Use ClientInterface to define client instance

--- failless_cli.cpp
+++ failless_cli.cpp
@@ -3,6 +3,6 @@
 
 int main(int argc, char **argv) {
     failless::client::config::ClientConfig test_interface_data("user", "SEND my.txt FROM .", 0, "key", "127.0.0.1", "11564");
-    failless::client::core::Client user = failless::client::core::Client(test_interface_data);
-    user.Run();
+    std::unique_ptr<failless::client::core::ClientInterface> client(new failless::client::core::Client(test_interface_data));
+    client.get()->Run();
 }
