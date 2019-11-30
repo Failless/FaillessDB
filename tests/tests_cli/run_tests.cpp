a9bdd0ebad4e29983840552a53dfac9d3542654e
Shampooh <agent.serega@yandex.ru>
2019-11-30 16:58:32 +0300

    Clean up main test startup file

--- tests/tests_cli/run_tests.cpp
+++ tests/tests_cli/run_tests.cpp
@@ -1,41 +1,17 @@
-#include <string>
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
-#include "llsscli/core/client.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "tests/tests_cli/test_serializer_mock.h"
+#include "tests/tests_cli/test_serializer_unit.h"
 
-namespace failless::client::tests {
+namespace failless {
+namespace client {
+namespace test {
 
-using ::testing::AtLeast;
-using ::testing::DoAll;
-using ::testing::Return;
-using ::testing::SetArgReferee;
-
-using std::string;
-
-using namespace failless::client::core;
-
-class MockClient : public Client {
- public:
-    explicit MockClient(config::ClientConfig &test_data) : Client(test_data){};
-    MOCK_METHOD1(ReadQuery, size_t(string query));
-    MOCK_METHOD0(Send, size_t());
-    MOCK_METHOD2(Params, size_t(int argc, char **arg));
-
- private:
-    MOCK_METHOD1(ParseQuery_, size_t(string query));
-};
-
-TEST(client_test, can_read_query) {
-    config::ClientConfig config;
-    MockClient mock_client(config);
-    string test_query = "test";
-    EXPECT_CALL(mock_client, ReadQuery(test_query)).Times(AtLeast(1));
-    EXPECT_EQ(mock_client.ParseInput_("test"), EXIT_SUCCESS);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleMock(&argc, argv);
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
-}  // namespace failless::client::tests
+}  // namespace test
+}  // namespace client
+}  // namespace failless
