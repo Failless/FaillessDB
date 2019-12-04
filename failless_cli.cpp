2d65cd43ff1d5345997df66a420b6519b5f3ca30
Shampooh <agent.serega@yandex.ru>
2019-12-05 01:03:32 +0300

    Add clients num for client init and add some req examples to implement

--- failless_cli.cpp
+++ failless_cli.cpp
@@ -2,7 +2,13 @@
 #include "llsscli/config/config.h"
 
 int main(int argc, char **argv) {
-    failless::client::config::ClientConfig test_interface_data("user", "SEND my.txt FROM .", 0, "key", "127.0.0.1", "11564");
+    failless::client::config::ClientConfig test_interface_data("user", "SEND my.txt FROM .", 0, "key", "127.0.0.1", "11564", 10);
     std::unique_ptr<failless::client::core::ClientInterface> client(new failless::client::core::Client(test_interface_data));
     client.get()->Run();
 }
+
+// SEND my.txt FROM . =>
+// SET key 'value'
+// GET key
+// CREATE (folder_id - optional else will be retured)
+// REGISTER login pass
