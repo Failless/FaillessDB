db3286c605284be14c51606880864b3d70ce8d69
Shampooh <agent.serega@yandex.ru>
2019-12-05 01:04:53 +0300

    Extend NetworkCLientInterface methods

--- llsscli/network/network_client_interface.h
+++ llsscli/network/network_client_interface.h
@@ -1,11 +1,11 @@
 #ifndef LLSSCLI_NETWORK_CLIENT_INTERFACE_H_
 #define LLSSCLI_NETWORK_CLIENT_INTERFACE_H_
 
-//https://stackoverflow.com/questions/12990840/boost-async-tcp-client
-//https://www.codeproject.com/Articles/1264257/Socket-Programming-in-Cplusplus-using-boost-asio-T
+// https://stackoverflow.com/questions/12990840/boost-async-tcp-client
+// https://www.codeproject.com/Articles/1264257/Socket-Programming-in-Cplusplus-using-boost-asio-T
 
-#include <boost/core/noncopyable.hpp>
 #include <boost/asio.hpp>
+#include <boost/core/noncopyable.hpp>
 
 namespace failless {
 namespace client {
@@ -14,21 +14,29 @@ namespace network {
 using boost::asio::ip::tcp;
 
 class NetworkClientInterface : boost::noncopyable {
-public:
+ public:
     virtual ~NetworkClientInterface() = default;
 
-    virtual size_t Open(std::shared_ptr<std::stringstream>& current_task) = 0;
+    virtual size_t AddUserTask(std::shared_ptr<std::stringstream>& current_task,
+                               std::shared_ptr<std::function<size_t()>>& callback) = 0;
+    virtual size_t OpenConnection() = 0;
     virtual size_t Close() = 0;
 
-private:
-    virtual void OnConnect_(const boost::system::error_code& error_code, tcp::resolver::iterator end_point_iter) = 0;
-    virtual void OnReceive_(const boost::system::error_code& error_code) = 0;
-    virtual void OnSend_(const boost::system::error_code& error_code) = 0;
-    virtual void DoClose_() = 0;
+ private:
+    virtual void OnConnect_(const boost::system::error_code& error_code,
+                            tcp::resolver::iterator end_point_iter,
+                            std::shared_ptr<tcp::socket>& socket,
+                            std::shared_ptr<std::stringstream>& current_task) = 0;
+    virtual void OnReceive_(const boost::system::error_code& ErrorCode,
+                            std::shared_ptr<tcp::socket>& socket) = 0;
+    virtual void OnSend_(const boost::system::error_code& error_code,
+                         std::shared_ptr<tcp::socket>& socket,
+                         std::shared_ptr<std::string>& str_task) = 0;
+    virtual void DoClose_(std::shared_ptr<tcp::socket>& socket) = 0;
 };
 
-} // namespace network
-} // namespace llsscli
-} // namespace failless
+}  // namespace network
+}  // namespace client
+}  // namespace failless
 
-#endif // LLSSCLI_NETWORK_CLIENT_INTERFACE_H_
+#endif  // LLSSCLI_NETWORK_CLIENT_INTERFACE_H_
