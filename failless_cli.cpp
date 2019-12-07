c792628d0131784fb1d061d4ae147e1bc1e22680
Shampooh <agent.serega@yandex.ru>
2019-12-07 13:15:08 +0300

    Add several test cases - SET GET CREATE REGISTER

--- failless_cli.cpp
+++ failless_cli.cpp
@@ -2,13 +2,18 @@
 #include "llsscli/config/config.h"
 
 int main(int argc, char **argv) {
-    failless::client::config::ClientConfig test_interface_data("user", "SEND my.txt FROM .", 0, "key", "127.0.0.1", "11564", 10);
-    std::unique_ptr<failless::client::core::ClientInterface> client(new failless::client::core::Client(test_interface_data));
+    // TODO [BUG] If payload = 0 => somewhere in stringstrema like \0 => stop of sending => lost data
+    failless::client::config::ClientConfig test_interface_send("user", "SEND my.txt FROM .", 1, "user_key", "127.0.0.1", "11564", 10);
+    failless::client::config::ClientConfig test_interface_set("user", "SET key", 1, "key_value", "127.0.0.1", "11564", 10);
+    failless::client::config::ClientConfig test_interface_get("user", "GET key", 1, "key_value_get", "127.0.0.1", "11564", 10);
+    failless::client::config::ClientConfig test_interface_create("user", "CREATE folder_id", 1, "", "127.0.0.1", "11564", 10);
+    failless::client::config::ClientConfig test_interface_register("user", "REGISTER login pass", 1, "", "127.0.0.1", "11564", 10);
+    std::unique_ptr<failless::client::core::ClientInterface> client(new failless::client::core::Client(test_interface_send));
     client.get()->Run();
 }
 
 // SEND my.txt FROM . =>
 // SET key 'value'
 // GET key
-// CREATE (folder_id - optional else will be retured)
+// CREATE (folder_id - optional else will be returned)
 // REGISTER login pass
