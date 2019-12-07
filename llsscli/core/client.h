e774a49234b325728475f301487a1c94ea9e2ed3
Shampooh <agent.serega@yandex.ru>
2019-12-07 13:12:02 +0300

    Define methods and callbacks for atomic user reqs

--- llsscli/core/client.h
+++ llsscli/core/client.h
@@ -1,6 +1,8 @@
 #ifndef LLSSCLIENT_CORE_CLIENT_H_
 #define LLSSCLIENT_CORE_CLIENT_H_
 
+#include <functional>
+
 #include "llsscli/config/config.h"
 #include "llsscli/filesystem/filesystem.h"
 #include "llsscli/network/network_client.h"
@@ -17,13 +19,24 @@ class Client : public ClientInterface {
     explicit Client(config::ClientConfig& test_data);
     ~Client() = default;
     size_t Run() override;
+    size_t ReadInput() override;
 
-    size_t SendRequestWithCB_(std::stringstream serialized_query,
-                              std::uintptr_t call_back) override;
-    size_t SerializeQuery_(std::string query) override;
+ private:
+    size_t SerializeQuery_() override;
     size_t ExecQuery_() override;
     size_t ParseInput_(std::string raw_query) override;
-    size_t ReadInput_(int argc, char** argv) override;
+
+    size_t SendToDb_() override;
+    size_t SetDBKey_() override;
+    size_t GetDBKey_() override;
+    size_t CreateDBFolder_() override;
+    size_t Register_() override;
+
+    size_t SendToDbCallback_() override;
+    size_t SetDBKeyCallback_() override;
+    size_t GetDBKeyCallback_() override;
+    size_t CreateDBFolderCallback_() override;
+    size_t RegisterCallback_() override;
 
  private:
     std::unique_ptr<network::NetworkClientInterface> network_client_;
@@ -41,6 +54,12 @@ class Client : public ClientInterface {
     bool parse_input_status_;
     bool exec_query_status_;
     bool serialize_query_status_;
+
+    std::shared_ptr<std::function<size_t()>> send_to_db_callback_;
+    std::shared_ptr<std::function<size_t()>> set_db_callback_;
+    std::shared_ptr<std::function<size_t()>> get_db_callback_;
+    std::shared_ptr<std::function<size_t()>> create_db_folder_callback_;
+    std::shared_ptr<std::function<size_t()>> register_callback_;
 };
 
 }  // namespace core
