ad2064a6384329a8255dd56c306a87bd9dba8ddc
Shampooh <agent.serega@yandex.ru>
2019-12-07 13:16:41 +0300

    Improve codestyle and add data size sending call as a header req

--- llsscli/network/network_client.cpp
+++ llsscli/network/network_client.cpp
@@ -10,26 +10,31 @@ namespace client {
 namespace network {
 
 NetworkClient::NetworkClient(std::shared_ptr<config::NetworkConfig>& config)
-    : config_(config.operator*()) {}
+    : config_(config.operator*()) {
+    content_buffer_vector_.resize(1024);
+    user_socket_queue_ =
+        std::make_shared<common::utils::Queue<std::shared_ptr<config::NetworkConnectTask>>>();
+}
 
 size_t NetworkClient::AddUserTask(std::shared_ptr<std::stringstream>& current_task,
                                   std::shared_ptr<std::function<size_t()>>& callback) {
+    // Init io_service & socket for one specific task
     std::shared_ptr<boost::asio::io_service> io_svc = std::make_shared<boost::asio::io_service>();
     std::shared_ptr<tcp::socket> socket = std::make_shared<tcp::socket>(*io_svc);
+
+    // Init network-like task
     std::shared_ptr<config::NetworkConnectTask> task =
         std::make_shared<config::NetworkConnectTask>(socket, io_svc, current_task, callback);
 
-    user_socket_queue_ =
-        std::make_shared<common::utils::Queue<std::shared_ptr<config::NetworkConnectTask>>>();
     user_socket_queue_->Push(task);
     return 0;
 }
 
 size_t NetworkClient::OpenConnection() {
-    while (!user_socket_queue_.operator*().IsEmpty()) {
+    while (!user_socket_queue_->IsEmpty()) {
         std::shared_ptr<config::NetworkConnectTask> temp = user_socket_queue_->Pop();
 
-        std::cout << "Client is starting... with data " << temp->client_task->str() << std::endl;
+        std::cout << "Client is starting..." << std::endl;
 
         tcp::resolver Resolver(*temp->io_service.get());
 
@@ -39,14 +44,13 @@ size_t NetworkClient::OpenConnection() {
         tcp::endpoint end_point = *EndPointIterator;
 
         temp->socket->async_connect(
-            end_point,
-            boost::bind(&NetworkClient::OnConnect_, this, boost::asio::placeholders::error,
-                        ++EndPointIterator, temp->socket, temp->client_task));
+            end_point, boost::bind(&NetworkClient::OnConnect_, this,
+                                   boost::asio::placeholders::error, ++EndPointIterator, temp));
 
         std::cout << "Client is started!" << std::endl;
 
         boost::thread ClientThread(
-            boost::bind(&boost::asio::io_service::run, temp.get()->io_service));
+            boost::bind(&boost::asio::io_service::run, temp->io_service));
 
         //                network_client_->Close();
         ClientThread.join();
@@ -55,9 +59,8 @@ size_t NetworkClient::OpenConnection() {
 }
 
 size_t NetworkClient::Close() {
-    while (!user_socket_queue_.operator*().IsEmpty()) {
+    while (!user_socket_queue_->IsEmpty()) {
         std::shared_ptr<config::NetworkConnectTask> temp = user_socket_queue_->Pop();
-
         temp->io_service->post(boost::bind(&NetworkClient::DoClose_, this, temp->socket));
     }
     return 0;
@@ -65,22 +68,36 @@ size_t NetworkClient::Close() {
 
 void NetworkClient::OnConnect_(const boost::system::error_code& ErrorCode,
                                tcp::resolver::iterator EndPointIter,
-                               std::shared_ptr<tcp::socket>& socket,
-                               std::shared_ptr<std::stringstream>& current_task) {
+                               std::shared_ptr<config::NetworkConnectTask>& task) {
     std::cout << "OnConnect..." << std::endl;
+
     if (ErrorCode.value() == boost::system::errc::success) {
-        std::shared_ptr<std::string> str_task(new std::string(current_task->str()));
-        std::cout << "Entered: " << current_task->str() << std::endl;
-        socket->async_write_some(boost::asio::buffer(current_task->str()),
-                                 boost::bind(&NetworkClient::OnSend_, this,
-                                             boost::asio::placeholders::error, socket, str_task));
+        std::shared_ptr<std::string> str_task =
+            std::shared_ptr<std::string>(new std::string(task->client_task->str()));
+        std::shared_ptr<std::string> str_task_len = std::shared_ptr<std::string>(
+            new std::string(std::to_string(task->client_task->str().length())));
+
+        std::cout << "Entered: " << *str_task_len << std::endl;
+
+        task->socket->async_write_some(
+            boost::asio::buffer(*str_task_len, str_task_len->length()),
+            boost::bind(&NetworkClient::OnSend_, this, boost::asio::placeholders::error,
+                        task->socket, str_task_len));
+
+        std::cout << "Entered: " << task->client_task->str() << std::endl;
+
+        task->socket->async_write_some(
+            boost::asio::buffer(*str_task, str_task->length()),
+            boost::bind(&NetworkClient::OnSend_, this, boost::asio::placeholders::error,
+                        task->socket, str_task));
+
     } else if (EndPointIter != tcp::resolver::iterator()) {
-        socket->close();
+        task->socket->close();
         tcp::endpoint EndPoint = *EndPointIter;
 
-        socket->async_connect(EndPoint, boost::bind(&NetworkClient::OnConnect_, this,
-                                                    boost::asio::placeholders::error,
-                                                    ++EndPointIter, socket, current_task));
+        task->socket->async_connect(
+            EndPoint, boost::bind(&NetworkClient::OnConnect_, this,
+                                  boost::asio::placeholders::error, ++EndPointIter, task));
     }
 }
 
@@ -88,11 +105,11 @@ void NetworkClient::OnReceive_(const boost::system::error_code& ErrorCode,
                                std::shared_ptr<tcp::socket>& socket) {
     std::cout << "receiving..." << std::endl;
     if (ErrorCode.value() == boost::system::errc::success) {
-        std::cout << recieve_buffer_ << std::endl;
+        std::cout << content_buffer_vector_.data() << "<-data" << std::endl;
 
-//        socket->async_read_some(boost::asio::buffer(recieve_buffer_, buflen_),
-//                                boost::bind(&NetworkClient::OnReceive_, this,
-//                                            boost::asio::placeholders::error, socket));
+        //        socket->async_read_some(boost::asio::buffer(recieve_buffer_, buflen_),
+        //                                boost::bind(&NetworkClient::OnReceive_, this,
+        //                                            boost::asio::placeholders::error, socket));
     } else {
         std::cout << "ERROR! OnReceive..." << std::endl;
         DoClose_(socket);
@@ -106,7 +123,7 @@ void NetworkClient::OnSend_(const boost::system::error_code& error_code,
     if (!error_code) {
         std::cout << "\"" << *str_task << "\" has been sent" << std::endl;
 
-        socket->async_receive(boost::asio::buffer(recieve_buffer_, buflen_),
+        socket->async_receive(boost::asio::buffer(content_buffer_vector_),
                               boost::bind(&NetworkClient::OnReceive_, this,
                                           boost::asio::placeholders::error, socket));
     } else {
