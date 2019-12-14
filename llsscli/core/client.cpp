63a342535f52b2378e7af0792865c4f0af8b6f1f
Shampooh <agent.serega@yandex.ru>
2019-12-14 14:20:24 +0300

    Viva console

--- llsscli/core/client.cpp
+++ llsscli/core/client.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "llss3p/enums/operators.h"
+
 #include "llsscli/core/client.h"
 #include "llsscli/utils/str_switch.h"
+#include <boost/algorithm/string.hpp>
 
 // https://www.modernescpp.com/index.php/c-core-guidelines-passing-smart-pointer#h2-get-your-e-book-at-leanpub
 // - про unique_ptr
@@ -14,7 +17,8 @@ Client::Client(config::ClientConfig& test_data) : config_(std::move(test_data)) 
     // TODO Check inits' status
     // Init FileSystem and Serializer interfaces
     filesystem_ = std::unique_ptr<filesystem::FileSystemInterface>(new filesystem::FileSystem());
-    serializer_ = std::unique_ptr<common::serializer::SerializerInterface<config::Task>>(new common::serializer::Serializer<config::Task>());
+    serializer_ = std::unique_ptr<common::serializer::SerializerInterface<common::utils::Packet>>(
+        new common::serializer::Serializer<common::utils::Packet>());
 
     // подумать над сверткой в 1 ф-ю
     // Init pointers to main callbacks
@@ -32,7 +36,6 @@ Client::Client(config::ClientConfig& test_data) : config_(std::move(test_data)) 
 
 size_t Client::Run() {
     // Split user request to tokens and check its status
-
     parse_input_status_ = ParseInput_(config_.user_request);
     if (parse_input_status_) {
         return -1;
@@ -41,7 +44,7 @@ size_t Client::Run() {
     // TODO Check init's status
     // Init base network configuration and init NetworkClient interface
     net_config_ = std::shared_ptr<config::NetworkConfig>(
-        new config::NetworkConfig(config_.db_host, config_.db_port, config_.client_num));
+        new config::NetworkConfig(config_.db_host, config_.db_port));
     network_client_ =
         std::unique_ptr<network::NetworkClientInterface>(new network::NetworkClient(net_config_));
 
@@ -62,18 +65,18 @@ size_t Client::Run() {
 
 size_t Client::SerializeQuery_() {
     // Serialize file container to stringstream and get it
-//    serializer_->Serialize(current_task_);
     serializer_->Serialize(*current_task_);
-
-    serialized_query_ = std::shared_ptr<std::stringstream>(new std::stringstream("хуй"));
     serialized_query_ = serializer_->GetOutStringStream();
-
     return 0;
 }
 
 size_t Client::ExecQuery_() {
+    std::cout<<query_tokens_[0]<<std::endl;
+
     std::for_each(query_tokens_[0].begin(), query_tokens_[0].end(),
                   [](char& c) { c = std::toupper(c); });
+
+    std::cout<<query_tokens_[0]<<std::endl;
     SWITCH(query_tokens_[0]) {
         CASE("SEND") : SendToDb_();
         break;
@@ -90,30 +93,19 @@ size_t Client::ExecQuery_() {
 }
 
 size_t Client::ParseInput_(std::string raw_query) {
-    std::string delimiter = " ";
-
-    size_t pos = 0;
-    int i = 0;
-    // 4 - max number of user req params
-    while ((pos = raw_query.find(delimiter)) != std::string::npos && i < 4) {
-        query_tokens_[i] = raw_query.substr(0, pos);
-        std::cout << query_tokens_[i] << std::endl;
-        raw_query.erase(0, pos + delimiter.length());
-        ++i;
-    }
+    boost::split(query_tokens_, raw_query, [](char c){return c == ' ';});
     return 0;
 }
 
 size_t Client::ReadInput() {
     std::string input;
-    std::cin >> input;
+    std::getline(std::cin, input);
     while (input != "exit") {
         config_.db_host = "127.0.0.1";
-        config_.db_port = "11564";
+        config_.db_port = "11556";
         config_.user_name = "user";
         config_.payload_dest_id = 1;
-        config_.payload_key = "1";
-        config_.client_num = 10;
+        config_.payload_key = "payload_key";
         config_.user_request = input;
         Run();
 
@@ -123,29 +115,51 @@ size_t Client::ReadInput() {
 }
 
 size_t Client::SendToDb_() {
+//    // Define file container and its size
+//    std::unique_ptr<std::vector<unsigned char>> value;
+//    std::streampos len;
+//
+//    // Fill file container with binary bytes
+//    filesystem_->ReadFile(query_tokens_[1], value, len);
+//    std::cout << value->data() << std::endl;
+//
+//    // Init user Data struct
+//    std::unique_ptr<std::string> key(new std::string(config_.payload_key));
+//    std::unique_ptr<config::Data> data(new config::Data(value, len, config_.payload_dest_id, key));
+//
+//    // Init user Task struct
+//    std::unique_ptr<std::string> user_name(new std::string(config_.user_name));
+//    std::unique_ptr<std::string> query(new std::string(config_.user_request));
+//    current_task_.reset(new config::Task(user_name, query, data));
+//    std::cout << current_task_->payload->value->data() << std::endl;
+//
+//    SerializeQuery_();
+    return 0;
+}
+
+size_t Client::SetDBKey_() {
     // Define file container and its size
     std::unique_ptr<std::vector<unsigned char>> value;
     std::streampos len;
 
     // Fill file container with binary bytes
-    filesystem_->ReadFile(query_tokens_[1], value, len);
-    std::cout << value.get()->data() << std::endl;
+    std::cout<<query_tokens_[2]<<std::endl;
+    filesystem_->ReadFile(query_tokens_[2], value, len);
 
     // Init user Data struct
-    std::unique_ptr<std::string> key(new std::string(config_.payload_key));
-    std::unique_ptr<config::Data> data(new config::Data(value, len, config_.payload_dest_id, key));
+    common::utils::Data data(1, len, *value);
 
     // Init user Task struct
-    std::unique_ptr<std::string> user_name(new std::string(config_.user_name));
-    std::unique_ptr<std::string> query(new std::string(config_.user_request));
-    current_task_.reset(new config::Task(user_name, query, data));
-    std::cout << current_task_.get()->payload.get()->value->data() << std::endl;
+    current_task_.reset(new common::utils::Packet());
+    current_task_->data = data;
+    current_task_->command = common::enums::operators::SET;
+    current_task_->login = "user";
+    current_task_->pass = "pass";
+    current_task_->ret_value = common::enums::response_type::NOT_SET;
 
     SerializeQuery_();
     return 0;
 }
-
-size_t Client::SetDBKey_() { return 0; }
 size_t Client::GetDBKey_() { return 0; }
 size_t Client::CreateDBFolder_() { return 0; }
 size_t Client::Register_() { return 0; }
