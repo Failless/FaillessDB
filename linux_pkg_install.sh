288843bad0196a33a832171abac0410845a9ef1c
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-12-23 15:55:49 +0300

    fixes in sh script

--- linux_pkg_install.sh
+++ linux_pkg_install.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+# cmake installation
+DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+mkdir "${DEPS_DIR}" && cd "${DEPS_DIR}" || exit
+CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v3.15.6/cmake-3.15.6.tar.gz"
+mkdir cmake
+travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+
 # rocksdb installation
+cd "${TRAVIS_BUILD_DIR}/deps" || exit
 sudo apt-get install libgflags-dev
 sudo apt-get install libsnappy-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install libbz2-dev
-sudo apt-get install liblz4-dev
+sudo apt-get install liblz4-devcmake
 sudo apt-get install libzstd-dev
 git clone https://github.com/facebook/rocksdb.git
 cd rocksdb || exit
 mkdir build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF -DWITH_TOOLS=OFF
+"${DEPS_DIR}"/cmake/bin/cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF -DWITH_TOOLS=OFF
 make all -j 8 # (TODO: change it on core num)
 sudo make install
 
